9aa942e089a1051bfeeee1386ed53ceb894248d8
PushkarMalhotra2006 <pushkarpmmal20@gmail.com>
2026-03-26 17:45:49 +0530

    made website ready

--- Deployment/Loan_app.py
+++ Deployment/Loan_app.py
@@ -9,6 +9,49 @@ columns = joblib.load("Loan_columns.pkl")
 st.title('Loan Approval Prediction')
 st.write("This website will predict whether your loan will be approved or not")
 
+st.header("📂 Batch Prediction")
+
+uploaded_file = st.file_uploader("Upload CSV for batch prediction", type=["csv"])
+if uploaded_file is not None:
+    df_test = pd.read_csv(uploaded_file)
+
+    df_test["Gender"] = df_test["Gender"].map({"Male":1, "Female":0})
+    df_test["Married"] = df_test["Married"].map({"Yes":1, "No":0})
+    df_test["Dependents"] = df_test["Dependents"].replace("3+", 3).astype(int)
+    df_test["Education"] = df_test["Education"].map({"Not Graduate":1, "Graduate":0})
+    df_test["Self_Employed"] = df_test["Self_Employed"].map({"Yes":1, "No":0})
+    df_test["Credit_History"] = df_test["Credit_History"].fillna(0)
+    df_test["Property_Area_Semiurban"] = (df_test["Property_Area"] == "Semiurban").astype(int)
+    df_test["Property_Area_Urban"] = (df_test["Property_Area"] == "Urban").astype(int)
+
+    df_test["Total_Income"] = df_test["ApplicantIncome"] + df_test["CoapplicantIncome"]
+
+    for col in columns:
+        if col not in df_test.columns:
+            df_test[col] = 0
+
+    df_test = df_test[columns]
+    df_test = df_test.values
+
+    df_scaled = scaler.transform(df_test)
+
+    preds = model.predict(df_scaled)
+
+    df_test["Prediction"] = preds.map({1: "Approved", 0: "Rejected"})
+
+    st.write(df_test)
+    
+    st.write("Download Here 🔽")
+    st.download_button(
+        "Download Results",
+        df_test.to_csv(index=False),
+        file_name="loan_predictions.csv"
+    )
+
+st.divider()
+
+st.header("🧾 Single Prediction")
+
 gender = st.selectbox("Gender",["Male","Female"])
 married = st.selectbox("Married", ["Yes", "No"])
 dependents = st.selectbox("Dependents", ["0", "1", "2", "3+"])
@@ -16,7 +59,7 @@ education = st.selectbox("Education", ["Graduate", "Not Graduate"])
 self_employed = st.selectbox("Self Employed", ["Yes", "No"])
 Applicantincome = st.number_input("Applicant Income",value=0)
 co_income= st.number_input("CoApplicant Income",value=0)
-loan_amount = st.number_input("Loan Amount",value=0)
+loan_amount = st.number_input("Loan Amount (In Thousands)",value=0)
 loan_term = st.number_input("Loan Amount Term",value=0)
 credit_history = st.selectbox("Credit History", ["Good", "Bad"])
 property_area = st.selectbox("Property Area", ["Urban", "Semiurban", "Rural"])
@@ -46,20 +89,13 @@ if st.button("Predict"):
             df[col]=0
 
     df= df[columns]
+    df = df.values
 
     df_scaled = scaler.transform(df)
 
-    prediction = model.predict(df)[0]
-
-    st.write("FINAL DF")
-    st.write(df)
-
-    st.write("SCALED DF")
-    st.write(df_scaled)
-
-    st.write("PREDICTION:", prediction)
+    prediction = model.predict(df_scaled)[0]
 
     if prediction==1:
-        st.success("Your Loan will be APPROVED")
+        st.success("Your Loan will be APPROVED ✅")
     else:
-        st.error("Your Loan will be REJECTED")
+        st.error("Your Loan will be REJECTED ❌")
